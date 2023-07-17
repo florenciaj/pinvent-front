12df6eb9af527745b0efdd14e8813b531b773f1e
florencialp <f.lopez.pacini@gmail.com>
2023-07-16 21:45:44 -0300

    add home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { RiProductHuntLine } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
+import HeroImg from '../../Assets/inv-img.png';
+import './Home.scss';
 
 const Home = () => {
-  return (
-    <div>Esto es el home</div>
-  )
+    return (
+        <div className='home'>
+            <nav className='container --flex-between'>
+                <div className="logo">
+                    <RiProductHuntLine size={35} />
+                </div>
+
+                <ul className="home-links">
+                    <li>
+                        <Link to='/register'>Register</Link>
+                    </li>
+                    <li>
+                        <button className="--btn --btn-primary">
+                            <Link to='/login'>Login</Link>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="--btn --btn-primary">
+                            <Link to='/dashboard'>Dashboard</Link>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+
+            <section className="container hero">
+                <div className="hero-text">
+                    <h2>Inventory {"&"} Stock Management Solution</h2>
+                    <p>
+                        Inventory system to control and manage proucts in the warehouse in
+                        real timeand integrated to make it easier to develop your business.
+                    </p>
+                    <div className="hero-buttons">
+                        <button className="--btn --btn-secondary">
+                            <Link to="/dashboard">Free Trial 1 Month</Link>
+                        </button>
+                    </div>
+                    <div className="--flex-start">
+                        <NumberText number="14K" text="Brand Owners" />
+                        <NumberText number="23K" text="Active Users" />
+                        <NumberText number="500+" text="Partners" />
+                    </div>
+                </div>
+
+                <div className="hero-image">
+                    <img src={HeroImg} alt="Inventory" />
+                </div>
+                <div className="hero-image"></div>
+            </section>
+        </div>
+    )
 }
 
-export default Home
+const NumberText = ({ number, text }) => {
+    return (
+        <div className="--mr">
+            <h3 className="--color-white">{number}</h3>
+            <h3 className="--color-white">{text}</h3>
+        </div>
+    )
+}
+
+export default Home;
